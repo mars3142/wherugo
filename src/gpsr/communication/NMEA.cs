d64e0b002179e01c82f33ed4b7fe9bd5ea8eedba
Peter Siegmund <developer@mars3142.org>
2012-02-14 21:51:20 +0100

    * [new] NMEA format description

--- src/gpsr/communication/NMEA.cs
+++ src/gpsr/communication/NMEA.cs
@@ -22,6 +22,11 @@ namespace org.mars3142.wherugo.Communication
 {
    public class NMEA
    {
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="nmea"></param>
       public void LoadData(String nmea)
       {
          if (nmea.Length == 0)
@@ -55,6 +60,18 @@ namespace org.mars3142.wherugo.Communication
 
             case "$GPBOD": // Bearing Origin to Destination
                ReadBOD(nmea);
+               break;
+
+            case "$GPBWC": // Bearing and Distance to Waypoint, Latitude, N/S, Longitude, E/W, UTC, Status
+               ReadBWC(nmea);
+               break;
+
+            case "$GPBWR": // Bearing and Distance to Waypoint - Rhumb Line, Latitude, N/S, Longitude, E/W, UTC, Status
+               ReadBWR(nmea);
+               break;
+
+            case "$GPBWW": // Bearing - Waypoint to Waypoint
+               ReadBWW(nmea);
                break;
          }
       }
@@ -263,6 +280,167 @@ namespace org.mars3142.wherugo.Communication
          // 6) FROM Waypoint 
          // 7) Checksum
          
+         return;
+      }
+
+      /// <summary>
+      /// Depth Below Keel
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadDPK(String nmea)
+      {
+         // 1) Depth, feet 
+         // 2) f = feet 
+         // 3) Depth, meters 
+         // 4) M = meters 
+         // 5) Depth, Fathoms 
+         // 6) F = Fathoms 
+         // 7) Checksum
+
+         return;
+      }
+
+      /// <summary>
+      /// Depth Below Surface
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadDBS(String nmea)
+      {
+         // 1) Depth, feet 
+         // 2) f = feet 
+         // 3) Depth, meters 
+         // 4) M = meters 
+         // 5) Depth, Fathoms 
+         // 6) F = Fathoms 
+         // 7) Checksum
+         
+         return;
+      }
+
+      /// <summary>
+      /// Depth below transducer
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadDBT(String nmea)
+      {
+         // 1) Depth, feet 
+         // 2) f = feet 
+         // 3) Depth, meters 
+         // 4) M = meters 
+         // 5) Depth, Fathoms 
+         // 6) F = Fathoms 
+         // 7) Checksum
+
+         return;
+      }
+
+      /// <summary>
+      /// Depth of water
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadDPT(String nmea)
+      {
+         // 1) Depth, meters 
+         // 2) Offset from transducer, 
+         //    positive means distance from tansducer to water line 
+         //    negative means distance from transducer to keel 
+         // 3) Checksum
+         
+         return;
+      }
+
+      /// <summary>
+      /// Datum Reference
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadDTM(String nmea)
+      {
+         // 1) Local datum code 
+         //    W84 - WGS84 
+         //    W72 - WGS72 
+         //    S85 - SGS85 
+         //    P90 - PE90 
+         //    999 - User defined 
+         //    IHO datum code 
+         // 2) Local datum sub code 
+         // 3) Latitude offset (minute) 
+         // 4) Latitude offset mark (N: +, S: -) 
+         // 5) Longitude offset (minute) 
+         // 6) Longitude offset mark (E: +, W: -) 
+         // 7) Altitude offset (m) Always null 
+         // 8) Datum  
+         //    W84 - WGS84 
+         //    W72 - WGS72 
+         //    S85 - SGS85 
+         //    P90 - PE90 
+         //    ... 
+         // 9) Checksum
+
+         return;
+      }
+
+      /// <summary>
+      /// Frequency Set Information
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadFSI(String nmea)
+      {
+         // 1) Transmitting Frequency 
+         // 2) Receiving Frequency 
+         // 3) Communications Mode (NMEA Syntax 2) 
+         // 4) Power Level 
+         // 5) Checksum
+
+         return;
+      }
+
+      /// <summary>
+      /// GPS Satellite Fault Detection
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadGBS(String nmea)
+      {
+         // 1) UTC time of the GGA fix addociated with this sentence 
+         // 2) Expected error in latitude 
+         // 3) Expected error in longitude 
+         // 4) Expected error in altitude 
+         // 5) Most likely failed satellite 
+         // 6) Probability of missed detection for most likely failed satellite 
+         // 7) Estimate of bias on most likely failed satellite 
+         // 8) Standard deviation on bias estimate 
+         // 9) Checksum
+
+         return;
+      }
+
+      /// <summary>
+      /// Global Positioning System Fix Data, Time, Position and fix related data fora GPS receiver.
+      /// </summary>
+      /// <param name="nmea"></param>
+      private void ReadGGA(String nmea)
+      {
+         //  1) Universal Time Coordinated (UTC) 
+         //  2) Latitude 
+         //  3) N or S (North or South) 
+         //  4) Longitude 
+         //  5) E or W (East or West) 
+         //  6) GPS Quality Indicator, 
+         //     0 - fix not available, 
+         //     1 - GPS fix, 
+         //     2 - Differential GPS fix 
+         //  7) Number of satellites in view, 00 - 12 
+         //  8) Horizontal Dilution of precision 
+         //  9) Antenna Altitude above/below mean-sea-level (geoid)  
+         // 10) Units of antenna altitude, meters 
+         // 11) Geoidal separation, the difference between the WGS-84 earth 
+         //     ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level 
+         //     below ellipsoid 
+         // 12) Units of geoidal separation, meters 
+         // 13) Age of differential GPS data, time in seconds since last SC104 
+         //     type 1 or 9 update, null field when DGPS is not used 
+         // 14) Differential reference station ID, 0000-1023 
+         // 15) Checksum
+
          return;
       }
    }
